ba62264aa18a717b7126b935bb42f9bb3f0d0cce
R.I.Pienaar <rip@devco.net>
2020-04-27 14:09:11 +0200

    improvements to the api schema examples

--- api/example_parse_event_test.go
+++ api/example_parse_event_test.go
@@ -5,27 +5,7 @@ import (
 )
 
 func ExampleParseEvent() {
-	// an event received from some medium like a NATS Subject or an event router
-	receivedEvent := `
-{
-  "type": "io.nats.jetstream.advisory.v1.api_audit",
-  "id": "uafvZ1UEDIW5FZV6kvLgWA",
-  "timestamp": "2020-04-23T16:51:18.516363Z",
-  "server": "NDJWE4SOUJOJT2TY5Y2YQEOAHGAK5VIGXTGKWJSFHVCII4ITI3LBHBUV",
-  "client": {
-    "host": "::1",
-    "port": 57924,
-    "cid": 17,
-    "account": "$G",
-    "name": "NATS CLI",
-    "lang": "go",
-    "version": "1.9.2"
-  },
-  "subject": "$JS.STREAM.LIST",
-  "response": "[\n  \"ORDERS\"\n]"
-}
-`
-
+	// receivedEvent was received over a transport like NATS, webhook or other medium
 	schema, event, err := ParseEvent([]byte(receivedEvent))
 	if err != nil {
 		panic(err.Error())

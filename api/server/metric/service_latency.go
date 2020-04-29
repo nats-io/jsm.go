ecf6e3a9bcf21b2d6c49bfc542e29b9b3659ef87
R.I.Pienaar <rip@devco.net>
2020-04-29 10:46:08 +0200

    update for latest service latency design
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- api/server/metric/service_latency.go
+++ api/server/metric/service_latency.go
@@ -9,21 +9,24 @@ import (
 //
 // NATS Schema Type io.nats.server.metric.v1.service_latency
 type ServiceLatencyV1 struct {
-	Type           string        `json:"type"`
-	ID             string        `json:"id"`
-	Time           time.Time     `json:"timestamp"`
-	Status         int           `json:"status"`
-	Error          string        `json:"description,omitempty"`
-	AppName        string        `json:"app,omitempty"`
-	RequestStart   time.Time     `json:"start"`
-	ServiceLatency time.Duration `json:"svc"`
-	NATSLatency    NATSLatencyV1 `json:"nats"`
-	TotalLatency   time.Duration `json:"total"`
+	Type           string          `json:"type"`
+	ID             string          `json:"id"`
+	Time           string          `json:"timestamp"`
+	Status         int             `json:"status"`
+	Error          string          `json:"description,omitempty"`
+	Requestor      LatencyClientV1 `json:"requestor,omitempty"`
+	Responder      LatencyClientV1 `json:"responder,omitempty"`
+	RequestStart   time.Time       `json:"start"`
+	ServiceLatency time.Duration   `json:"service"`
+	SystemLatency  time.Duration   `json:"system"`
+	TotalLatency   time.Duration   `json:"total"`
 }
 
-// NATSLatencyV1 represents the internal NATS latencies, including RTTs to clients.
-type NATSLatencyV1 struct {
-	Requestor time.Duration `json:"req"`
-	Responder time.Duration `json:"resp"`
-	System    time.Duration `json:"sys"`
+type LatencyClientV1 struct {
+	User   string        `json:"user,omitempty"`
+	Name   string        `json:"name,omitempty"`
+	RTT    time.Duration `json:"rtt"`
+	IP     string        `json:"ip"`
+	CID    uint64        `json:"cid"`
+	Server string        `json:"server"`
 }

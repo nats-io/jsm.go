97f93951b4651f8253c82d943b37f7107fc3ac8a
jnmoyne <jnmoyne@gmail.com>
2022-08-12 16:03:42 -0700

    Upgrade test to new version of ContainedSubjects

--- streams_test.go
+++ streams_test.go
@@ -928,13 +928,13 @@ func TestStream_ContainedSubjects(t *testing.T) {
 
 	subs, err := s.ContainedSubjects()
 	checkErr(t, err, "contained failed")
-	if !reflect.DeepEqual(subs, []string{"test.otherset.e1", "test.set.e1", "test.set.e2", "test.set.e3", "test.set.e4"}) {
+	if !reflect.DeepEqual(subs, []jsm.SubjectInfo{{"test.otherset.e1", 1}, {"test.set.e1", 1}, {"test.set.e2", 1}, {"test.set.e3", 1}, {"test.set.e4", 1}}) {
 		t.Fatalf("Invalid set: %v", subs)
 	}
 
 	subs, err = s.ContainedSubjects("test.set.>")
 	checkErr(t, err, "contained failed")
-	if !reflect.DeepEqual(subs, []string{"test.set.e1", "test.set.e2", "test.set.e3", "test.set.e4"}) {
+	if !reflect.DeepEqual(subs, []jsm.SubjectInfo{{"test.set.e1", 1}, {"test.set.e2", 1}, {"test.set.e3", 1}, {"test.set.e4", 1}}) {
 		t.Fatalf("Invalid set: %v", subs)
 	}
 }

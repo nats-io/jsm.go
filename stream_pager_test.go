ca7cf50d5592256be29c13b4571cca087e6f7e4a
R.I.Pienaar <rip@devco.net>
2024-04-10 16:57:06 +0200

    Fix viewing work queues, except with time deltas
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- stream_pager_test.go
+++ stream_pager_test.go
@@ -30,7 +30,6 @@ func TestPager(t *testing.T) {
 	if err != nil {
 		t.Fatalf("pager creation failed: %s", err)
 	}
-	defer pgr.Close()
 
 	seen := 0
 	pages := 0
@@ -62,5 +61,60 @@ func TestPager(t *testing.T) {
 	if len(known) != 0 {
 		t.Fatalf("expected no consumers got %v", known)
 	}
+}
 
+func TestPagerWQ(t *testing.T) {
+	srv, nc, mgr := startJSServer(t)
+	defer srv.Shutdown()
+	defer nc.Flush()
+
+	str, err := mgr.NewStream("PAGERTEST", jsm.Subjects("js.in.pager"), jsm.WorkQueueRetention(), jsm.AllowDirect())
+	if err != nil {
+		t.Fatalf("stream create failed: %s", err)
+	}
+
+	_, err = str.NewConsumer(jsm.ConsumerName("PULL"))
+	if err != nil {
+		t.Fatalf("consumer create failed: %s", err)
+	}
+
+	for i := 1; i <= 200; i++ {
+		_, err = nc.Request("js.in.pager", []byte(fmt.Sprintf("message %d", i)), time.Second)
+		if err != nil {
+			t.Fatalf("publish failed: %s", err)
+		}
+	}
+
+	_, err = str.PageContents(jsm.PagerSize(25), jsm.PagerStartDelta(time.Hour))
+	if err == nil || err.Error() != "workqueue paging does not support time delta starting positions" {
+		t.Fatalf("pager creation did not fail for time delta: %v", err)
+	}
+
+	pgr, err := str.PageContents(jsm.PagerSize(25))
+	if err != nil {
+		t.Fatalf("pager creation failed: %s", err)
+	}
+
+	seen := 0
+	pages := 0
+	for {
+		_, last, err := pgr.NextMsg(context.Background())
+		if err != nil && last && seen == 200 && pages == 8 {
+			break
+		}
+
+		if err != nil {
+			t.Fatalf("next failed seen %d pages %d: %s", seen, pages, err)
+		}
+
+		seen++
+		if last {
+			pages++
+		}
+	}
+
+	err = pgr.Close()
+	if err != nil {
+		t.Fatalf("close failed")
+	}
 }

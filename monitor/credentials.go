357c187f35819fff14e979240d33e9f975590304
R.I.Pienaar <rip@devco.net>
2024-08-06 10:57:33 +0300

    Extract cli credential check into a reusable package
    Signed-off-by: R.I.Pienaar <rip@devco.net>

---
+++ monitor/credentials.go
@@ -0,0 +1,80 @@
+// Copyright 2024 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package monitor
+
+import (
+	"os"
+	"time"
+
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nkeys"
+)
+
+type CredentialCheckOptions struct {
+	File             string        `json:"file" yaml:"file"`
+	ValidityWarning  time.Duration `json:"validity_warning" yaml:"validity_warning"`
+	ValidityCritical time.Duration `json:"validity_critical" yaml:"validity_critical"`
+	RequiresExpiry   bool          `json:"requires_expiry" yaml:"requires_expiry"`
+}
+
+func CheckCredential(check *Result, opts CredentialCheckOptions) error {
+	ok, err := fileAccessible(opts.File)
+	if err != nil {
+		check.Critical("credential not accessible: %v", err)
+		return nil
+	}
+
+	if !ok {
+		check.Critical("credential not accessible")
+		return nil
+	}
+
+	cb, err := os.ReadFile(opts.File)
+	if err != nil {
+		check.Critical("credential not accessible: %v", err)
+		return nil
+	}
+
+	token, err := nkeys.ParseDecoratedJWT(cb)
+	if err != nil {
+		check.Critical("invalid credential: %v", err)
+		return nil
+	}
+
+	claims, err := jwt.Decode(token)
+	if err != nil {
+		check.Critical("invalid credential: %v", err)
+	}
+
+	now := time.Now().UTC().Unix()
+	cd := claims.Claims()
+	until := cd.Expires - now
+	crit := int64(opts.ValidityCritical.Seconds())
+	warn := int64(opts.ValidityWarning.Seconds())
+
+	check.Pd(&PerfDataItem{Help: "Expiry time in seconds", Name: "expiry", Value: float64(until), Warn: float64(warn), Crit: float64(crit), Unit: "s"})
+
+	switch {
+	case cd.Expires == 0 && opts.RequiresExpiry:
+		check.Critical("never expires")
+	case opts.ValidityCritical > 0 && (until <= crit):
+		check.Critical("expires sooner than %s", f(opts.ValidityCritical))
+	case opts.ValidityWarning > 0 && (until <= warn):
+		check.Warn("expires sooner than %s", f(opts.ValidityWarning))
+	default:
+		check.Ok("expires in %s", time.Unix(cd.Expires, 0).UTC())
+	}
+
+	return nil
+}
